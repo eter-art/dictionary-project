c2658726eaddf4c90fa364d9625d0ce042275bae
eter-art <77591625+eter-art@users.noreply.github.com>
2021-05-28 13:46:07 +0200

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
 import './App.css';
+import Dictionary from "./Dictionary";
+
 
  export default function App() {
   return (
     <div className="App">
-     <header className="App-header"></header>
-    <h1>
-           Hallo
-    </h1>
+      <div className="container">
+     <header className="App-header"> </header>
+<main>
+    <Dictionary/>
+  </main> 
+   <footer className="App-footer"><small>coded by Eter Artmeladze</small></footer>
+ </div>
  </div>
   );
 }

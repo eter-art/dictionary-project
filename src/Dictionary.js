c836462fd81461d8cf29ff5b20af354f4e62c569
eter-art <77591625+eter-art@users.noreply.github.com>
2021-06-02 22:36:36 +0200

    added Photos to dictionary Project

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,36 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary (props){
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
-
-    function handleResponse (response){
+    let [photos, setPhotos] = useState(null);
+ // eslint-disable-next-line
+    function handleDictionResponse (response){
         setResults(response.data[0]);
     }
 
-    function search () {
-   // eslint-disable-next-line
 
-        let apiUrl = ` https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-         // eslint-disable-next-line
-        axios.get(apiUrl). then(handleResponse);
+     function handlePexelsResponse(response){
+     setPhotos(response.data.photos);
+     }
+
+
+    function search () {
+  
+   let apiUrl = ` https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+  // eslint-disable-next-line
+    axios.get(apiUrl). then(handleDictionResponse);
+
+     let pexelsApiKey= "563492ad6f91700001000001666dd97fd0df4d40a0ece4b2239244d0";
+     let pexelsApiUrl =  `https://api.pexels.com/v1/search?query=${keyword} &per_page=3`;
+     let headers = {"Authorization" : `Bearer ${pexelsApiKey}`} 
+     // eslint-disable-next-line
+      axios.get(pexelsApiUrl, { headers: headers} ). then(handlePexelsResponse);
     }
 
     function handleSubmit (event) {
@@ -47,6 +60,7 @@ if  (loaded){
             </form>
             </section>
             <Results results={results} />
+            <Photos photos = {photos} />
         </div>
     );
 } else{
